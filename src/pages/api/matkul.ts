7c39fea86ffbd9e187228182cafa83a13bde2c19
LordRonz <aarontanhar2000@gmail.com>
2021-12-31 12:29:50 +0700

    feat: update data matkul

--- src/pages/api/matkul.ts
+++ src/pages/api/matkul.ts
@@ -1,10 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+import type { DataMatkul } from '@/data/dataMatkul';
+import dataMatkul from '@/data/dataMatkul';
+
 type Data = {
-  name: string;
+  dataMatkul: DataMatkul[];
 };
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  res.status(200).json({ name: 'John Doe' });
+  res.status(200).json({ dataMatkul });
 }
