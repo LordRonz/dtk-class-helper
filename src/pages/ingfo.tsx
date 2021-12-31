af90c6a430ff588b2abb3a4efc1e8b2f157e961f
LordRonz <aarontanhar2000@gmail.com>
2021-12-31 17:29:43 +0700

    feat: ingfo page

---
+++ src/pages/ingfo.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable @next/next/no-img-element */
+import type { NextPage } from 'next';
+
+import Footer from '@/components/Footer';
+import ArrowLink from '@/components/links/ArrowLink';
+import CustomLink from '@/components/links/CustomLink';
+import Seo from '@/components/Seo';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Seo />
+      <main>
+        <section className='bg-black text-primary-50'>
+          <div className='min-h-screen py-16 space-y-10 text-primary-50 layout'>
+            <div className='space-y-4'>
+              <h1 className='text-primary-200'>Ingfo</h1>
+              <p>
+                Situs ini {'"'}terinspirasi{'"'} dari punyanya informatika, buatannya si{' '}
+                <CustomLink href='https://theodorusclarence.com'>Clarence</CustomLink>. Nih{' '}
+                <CustomLink href='https://class.thcl.dev/'>linknya</CustomLink>
+              </p>
+              <p>Kalau ada bug atau saran fitur bisa PR ke repo berikut:</p>
+              <ArrowLink href='https://github.com/lordronz/dtk-class-helper'>Source code</ArrowLink>
+              <p>Atau pm aja sih ya</p>
+              <h4>Data matkul</h4>
+              <ArrowLink href='/api/matkul' openNewTab={true}>
+                Link API JSON
+              </ArrowLink>
+            </div>
+            <div className='flex justify-center items-center'>
+              <CustomLink href='/' className='text-primary-200'>
+                Balik ke Home
+              </CustomLink>
+            </div>
+          </div>
+          <Footer />
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default Home;
