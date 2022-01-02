cd8f72e679ccff70b6fb353a7236a736f2952ef0
LordRonz <aarontanhar2000@gmail.com>
2022-01-02 19:39:03 +0700

    fix: accessibility only li are allowed inside ul

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -16,15 +16,17 @@ const Nav = (): JSX.Element => {
             <a className='font-bold text-white hover:text-primary-300 transition-all duration-200'>Home</a>
           </Link>
         </li>
-        <ul className='flex items-center justify-between space-x-4'>
-          {links.map(({ href, label }) => (
-            <li key={`${href}${label}`}>
-              <UnstyledLink href={href} className='text-white hover:text-primary-300 transition-all duration-200'>
-                {label}
-              </UnstyledLink>
-            </li>
-          ))}
-        </ul>
+        <li>
+          <ul className='flex items-center justify-between space-x-4'>
+            {links.map(({ href, label }) => (
+              <li key={`${href}${label}`}>
+                <UnstyledLink href={href} className='text-white hover:text-primary-300 transition-all duration-200'>
+                  {label}
+                </UnstyledLink>
+              </li>
+            ))}
+          </ul>
+        </li>
       </ul>
     </nav>
   );
