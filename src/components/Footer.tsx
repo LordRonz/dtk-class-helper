267f2088ae26c607e8f58aba1f129b6bb8504e3f
LordRonz <aarontanhar2000@gmail.com>
2022-01-06 17:55:22 +0700

    feat: change copyright

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,3 @@
-const Footer = (): JSX.Element => <footer className='text-center'>© Aaron Christopher {new Date().getFullYear()}</footer>;
+const Footer = (): JSX.Element => <footer className='text-center'>© Bukan siapa-siapa {new Date().getFullYear()}</footer>;
 
 export default Footer;
