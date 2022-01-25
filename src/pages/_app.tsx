4e9a4f730410e53ff1d7a7fedb82a7bcce5bfdf0
LordRonz <aarontanhar2000@gmail.com>
2022-01-25 12:46:22 +0700

    feat: add nprogress

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,19 @@
 import '@/styles/globals.css';
+import '@/styles/nprogress.css';
 
 import type { AppProps } from 'next/app';
+import Router from 'next/router';
 import { ThemeProvider } from 'next-themes';
+import nProgress from 'nprogress';
 import { useEffect } from 'react';
 import { hotjar } from 'react-hotjar';
 
 // hotjar config
 const [hjid, hjsv] = [2768716, 6];
+
+Router.events.on('routeChangeStart', nProgress.start);
+Router.events.on('routeChangeError', nProgress.done);
+Router.events.on('routeChangeComplete', nProgress.done);
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   useEffect(() => {
