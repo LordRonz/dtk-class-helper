e739bb50e11cba13d9cec685fa1122f4a5080107
LordRonz <aarontanhar2000@gmail.com>
2022-05-06 10:14:37 +0700

    fix: color mode toggle color value type :rocket:

--- src/components/ColorModeToggle.tsx
+++ src/components/ColorModeToggle.tsx
@@ -7,8 +7,8 @@ type Props = {
   buttonClassName?: string;
   className?: string;
   iconClassName?: string;
-  value: 'dark' | 'light';
-  onChange: (v: 'dark' | 'light') => void;
+  value?: string;
+  onChange: (v: string) => void;
 };
 
 const ColorModeToggle = ({ buttonClassName, className, iconClassName, value, onChange }: Props): JSX.Element => {
