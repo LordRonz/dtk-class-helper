80bb4c5e5bdc6a461f3b192a58f630c36ffe5389
LordRonz <aarontanhar2000@gmail.com>
2022-06-02 22:19:02 +0700

    fix: cypress config :rocket:

--- cypress.config.ts
+++ cypress.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      this.pageLoadTimeout = 120000;
     },
   },
+  pageLoadTimeout: 120000,
 });
